a9731e62784b0b10de724ce4ad6c9f87c792a78d
dt360-sussex <dt360@sussex.ac.uk>
2025-12-16 12:15:18 +0000

    Updated pyproject.toml

--- Agilent 33600A waveform.py
+++ Agilent 33600A waveform.py
@@ -6,7 +6,7 @@
 
 
 from pylablib.devices import AWG
-
+import numpy as np
 
 ip = 'TCPIP::169.254.11.23::INSTR'
 
@@ -14,14 +14,17 @@ ip = 'TCPIP::169.254.11.23::INSTR'
 
 class Agilent33600A(AWG.GenericAWG):
     """Minimal driver wrapper for Keysight/Agilent 33600A series AWGs."""
+    _default_operation_timeout = 10.0
+    _default_backend_timeout = 5.0
+    
     def __init__(self, addr):
         self._channels_number = 2  # 33600A has 2 channels
         super().__init__(addr)
-
+        
 
 awg=Agilent33600A(ip)
 
-print(awg.set_function("arb", 2))
+print(awg.set_function("square", 2))
 
-awg.write()
+
 awg.close()
